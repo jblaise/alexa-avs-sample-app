c798761a39dbe1515913aed1dedab75971c5512f
jblaise@cap-tic.fr <jblaise@cap-tic.fr>
2017-02-17 15:05:22 +0100

    extends installation to ubuntu 16 for rpi

--- samples/javaclient/install-java8.sh
+++ samples/javaclient/install-java8.sh
@@ -5,9 +5,12 @@ if [ -n "$(which java)" ] && [ -n "$(which javac)" ] && [ -n "$(grep webupd8team
 	exit 0
 fi
 
+OS=$(lsb_release -d 2>/dev/null | cut -f2 | cut -f1 -d' ')
+echo $OS
+
 # Ensure we are running on Raspbian
 lsb_release -a 2>/dev/null | grep Raspbian
-if [ "$?" -ne "0" ]; then
+if [ "$?" -ne "0" ] && [ "$OS" != "Ubuntu" ]; then
     echo "This OS is not Raspbian. Exiting..."
     exit 1
 fi
@@ -16,7 +19,9 @@ fi
 VERSION=`lsb_release -c 2>/dev/null | awk '{print $2}'`
 echo "Version of Raspbian determined to be: $VERSION"
 
-if [ "$VERSION" == "jessie" ]; then
+if [ "$OS" == "Ubuntu" ];then
+	UBUNTU_VERSION=$VERSION
+elif [ "$VERSION" == "jessie" ]; then
     UBUNTU_VERSION="trusty"
 elif [ "$VERSION" == "wheezy" ]; then
     UBUNTU_VERSION="precise"
@@ -31,8 +36,11 @@ sudo apt-get -y remove --purge oracle-java8-jdk oracle-java7-jdk openjdk-7-jre o
 
 # Install Java from Ubuntu's PPA
 # http://linuxg.net/how-to-install-the-oracle-java-8-on-debian-wheezy-and-debian-jessie-via-repository/
-sudo sh -c "echo \"deb http://ppa.launchpad.net/webupd8team/java/ubuntu $UBUNTU_VERSION main\" >> /etc/apt/sources.list"
-sudo sh -c "echo \"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu $UBUNTU_VERSION main\" >> /etc/apt/sources.list"
+grep webupd8team /etc/apt/sources.list >/dev/null
+if [ "$?" -ne "0" ]; then
+	sudo sh -c "echo \"deb http://ppa.launchpad.net/webupd8team/java/ubuntu $UBUNTU_VERSION main\" >> /etc/apt/sources.list"
+	sudo sh -c "echo \"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu $UBUNTU_VERSION main\" >> /etc/apt/sources.list"
+fi
 
 KEYSERVER=(pgp.mit.edu keyserver.ubuntu.com)
 
